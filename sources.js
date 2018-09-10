0c2ea22b6bf223c00492473d9594ed17bcd13351
emerytan <emery.t.anderson@gmail.com>
2018-09-10 17:15:57 -0400

    updated sources module

--- sources.js
+++ sources.js
@@ -13,5 +13,32 @@ module.exports = (io) => {
 			bmdRouter.write(Buffer.from('\n'))
 		})
 
+		bmdRouter.on('data', (data) => {
+			console.log(data.toString());
+			remaining += data
+			var index = remaining.indexOf('\n')
+			while (index > -1) {
+				var line = remaining.substring(0, index)
+				remaining = remaining.substring(index + 1)
+				parseData(line)
+				index = remaining.indexOf('\n')
+			}
+			remaining = ''
+		})
+
 	})
+}
+
+
+function parseData(data) {
+	var arr = []
+	var labelReg = /(^[0-9]{1,2})\s(.{3,})/
+	var ioReg = /(^[0-9]{1,2})\s([0-9]{1,2})/
+	var thisMatchinfo = data
+	arr = thisMatchinfo.split(' ')
+	console.log(arr);
+	var thisMatchinfo = routerText.write(data)
+	var found = thisMatchinfo.match(labelReg)
+	var currentRoutes = thisMatchinfo.match(ioReg)
+
 }
