b68216381b1a1f503c97fa2685d5757e0e93c33b
tarman <tarman@localhost.localdomain>
2019-01-10 16:02:53 -0500

    remove test files

--- destinations.js
+++
@@ -1,8 +0,0 @@
-module.exports = (io) => {
-    io.on('connection', (socket) => {
-        console.log('destinations socke io initialized...')
-        socket.emit('server messages', 'destinations running')
-    })
-}
-
-
--- test.js
+++
@@ -1,7 +0,0 @@
-var selectors = document.querySelectorAll('select')
-selectors.forEach((element) => {
-    element.addEventListener('change', (event) => {
-        var src = parseInt(event.target.value)
-        alert(`source: ${src}`)
-    })
-})
